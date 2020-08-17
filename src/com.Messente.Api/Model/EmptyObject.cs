3132f45d24c7f38d3e7958dffc108500c0bda1c1
api-librarian <development@messente.com>
2020-08-17 06:48:55 +0000

    Travis update: Aug 2020 (Build 617)
    [skip ci]

--- src/com.Messente.Api/Model/EmptyObject.cs
+++ src/com.Messente.Api/Model/EmptyObject.cs
@@ -23,7 +23,7 @@ using OpenAPIDateConverter = com.Messente.Api.Client.OpenAPIDateConverter;
 namespace com.Messente.Api.Model
 {
     /// <summary>
-    /// Response object without attributes
+    /// An object containing no properties
     /// </summary>
     [DataContract]
     public partial class EmptyObject :  IEquatable<EmptyObject>
